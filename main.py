b31a98bf38e43edfff357e3b8ee00d878660285d
Writer42 <writer42@mail.ru>
2023-01-19 21:30:36 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+@app.get("/")
+async def index():
+    page = open("./index.html", "r").read()
+    return HTMLResponse(page)
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    counter = 1
+    while True:
+        data = await websocket.receive_json()
+        data["listObject"] = f'{counter}. ' + data["listObject"]
+        await websocket.send_json(data)
+        counter += 1
